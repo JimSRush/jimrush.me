312b4fdbe5a6a3e49506462b13e7fa4559fd2e50
Jim Rush <jim@galesburg.co.nz>
2020-01-28 11:00:33 +1300

    add easter egg

---
+++ assets/script.js
@@ -0,0 +1,2 @@
+console.log("Ironically, for a job application that has a major javascript component, there didn't seem to be a need to write any. Apart from this.");
+console.log("Anyway, thanks for reading!");
