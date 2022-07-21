1bf33a5cc5d0031946ff9bfcf7b23aabbc64c5d8
Jim Rush <jim.rush@zxsecurity.co.nz>
2022-07-21 13:07:43 +1200

    remove stuff

--- assets/script.js
+++
@@ -1,2 +0,0 @@
-console.log("Ironically, for a job application that has a major javascript component, there didn't seem to be a need to write any. Apart from this.");
-console.log("Anyway, thanks for reading!");
